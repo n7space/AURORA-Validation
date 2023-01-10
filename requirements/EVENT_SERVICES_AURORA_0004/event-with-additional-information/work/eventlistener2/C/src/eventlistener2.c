764825192e1733f1d71535ede00f92430c6e4ef4
Filip Florczyk <filip.florczyk@n7space.com>
2023-01-10 15:49:46 +0100

    Refactored event with additional info test

--- requirements/EVENT_SERVICES_AURORA_0004/event-with-additional-information/work/eventlistener2/C/src/eventlistener2.c
+++ requirements/EVENT_SERVICES_AURORA_0004/event-with-additional-information/work/eventlistener2/C/src/eventlistener2.c
@@ -8,9 +8,7 @@
     !! file. The up-to-date signatures can be found in the .ads file.   !!
 */
 #include "eventlistener2.h"
-#include <stdio.h>
-
-#define SEC_TO_NS(sec) ((sec)*1000000000)
+#include "event_helper.h"
 
 void eventlistener2_startup(void)
 {
@@ -25,48 +23,7 @@ void eventlistener2_PI_receive_event
       (const asn1SccT_EventMessage *IN_eventmessage)
 
 {
-    uint64_t time = 0;
-    uint32_t seconds = 0;
-    uint32_t nanoseconds = 0;
-
-    seconds += (uint32_t)IN_eventmessage->timestamp.c1 << 24;
-    seconds += (uint32_t)IN_eventmessage->timestamp.c2 << 16;
-    seconds += (uint32_t)IN_eventmessage->timestamp.c3 << 8;
-    seconds += (uint32_t)IN_eventmessage->timestamp.c4;
-
-    nanoseconds += (uint32_t)IN_eventmessage->timestamp.f1 << 24;
-    nanoseconds += (uint32_t)IN_eventmessage->timestamp.f2 << 16;
-    nanoseconds += (uint32_t)IN_eventmessage->timestamp.f3 << 8;
-
-    time = SEC_TO_NS((uint64_t)seconds) + (uint64_t)nanoseconds;
-
-    printf("Event received by Listener 2:\n");
-    printf("  event id =       %ld\n", IN_eventmessage->id);
-    switch(IN_eventmessage->eventType)
-    {
-        case T_EventMessage_eventType_informative:
-        {
-            printf("  eventType =      informative\n");
-            break;
-        }
-        case T_EventMessage_eventType_lowSeverity:
-        {
-            printf("  eventType =      low severity\n");
-            break;
-        }
-        case T_EventMessage_eventType_mediumSeverity:
-        {
-            printf("  eventType =      medium severity\n");
-           break;
-        }
-        case T_EventMessage_eventType_highSeverity:
-        {
-            printf("  eventType =      high severity\n");
-            break;
-        }
-    }
-    printf("  message =        %s\n", IN_eventmessage->message.arr);
-    printf("  timestamp =      %ld\n\n", time);
+    print_event(2, IN_eventmessage);
 }
 
 
