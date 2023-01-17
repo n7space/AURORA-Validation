823b207b0ad1918463dfb75e088026169d2a1724
Filip Florczyk <filip.florczyk@n7space.com>
2023-01-17 09:36:40 +0100

    Added ASN.1 Integer data type test

---
+++ requirements/AUR_CBI_INT-0140/integer-param-test/work/qgenc/QGenC/src/Simulink_DataView_asn.m
@@ -0,0 +1,24 @@
+MyInteger = Simulink.AliasType;
+MyInteger.BaseType = 'uint16';
+MyInteger.Description = 'range is (0, 10000)';
+
+T_Int32 = Simulink.AliasType;
+T_Int32.BaseType = 'int32';
+T_Int32.Description = 'range is (-2147483648, 2147483647)';
+
+T_UInt32 = Simulink.AliasType;
+T_UInt32.BaseType = 'uint32';
+T_UInt32.Description = 'range is (0, 4294967295)';
+
+T_Int8 = Simulink.AliasType;
+T_Int8.BaseType = 'int8';
+T_Int8.Description = 'range is (-128, 127)';
+
+T_UInt8 = Simulink.AliasType;
+T_UInt8.BaseType = 'uint8';
+T_UInt8.Description = 'range is (0, 255)';
+
+T_Boolean = Simulink.AliasType;
+T_Boolean.BaseType = 'boolean';
+T_Boolean.Description = 'A simple BOOLEAN';
+
