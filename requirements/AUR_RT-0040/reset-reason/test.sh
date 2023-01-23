230c255ddca0e2c4dcdb639cf809b86add0b9a32
Michal Kurowski <mkurowski@n7space.com>
2023-01-23 13:49:10 +0100

    Review fixes

--- requirements/AUR_RT-0040/reset-reason/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo -e "Building binary\n"
-make leon3 debug
-echo -e "\nExecuting binary\n"
-sis-2.29 -leon3 -dumbio -uart1 system.log -rt work/binaries/leon3 < gdb.batch
-echo -e "\nTest result\n"
-cat system.log
